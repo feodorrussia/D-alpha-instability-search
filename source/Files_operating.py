8527a77ee58fd5bd903d1947d75ca6e9c30644e5
Fyodor <fvdelows@mail.ru>
2024-07-23 18:13:33 +0300

    Write func to rewrite files from entire dir Add checking result dir existing

--- source/Files_operating.py
+++ source/Files_operating.py
@@ -1,11 +1,15 @@
 import numpy as np
 import pandas as pd
+import os
 
 import shtReader_py.shtRipper as shtRipper
 
 
 def sht_rewrite(filepath='D:\Edu\Lab\D-alpha-instability-search/data/sht/', filename="sht44168",
                 result_path="../data/d-alpha/", result_format="txt"):
+    if not os.path.exists(result_path):
+        os.mkdir(result_path)
+
     try:
         if ".sht" in filename.lower():
             res = shtRipper.ripper.read(filepath + filename)
@@ -19,3 +23,15 @@ def sht_rewrite(filepath='D:\Edu\Lab\D-alpha-instability-search/data/sht/', file
         return "ok"
     except Exception as e:
         return e
+
+
+def rewrite_sht_fromDir(dir_path, result_path="../data/d-alpha/", result_format="txt"):
+    print("|", end="")
+
+    for name in os.listdir(dir_path):
+        report = sht_rewrite(filepath=dir_path, filename=name, result_path=result_path, result_format=result_format)
+
+        if report == "ok":
+            print(".", end="")
+        else:
+            print("-", end="")
